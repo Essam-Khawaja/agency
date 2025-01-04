31469cc84b0fa150a5b522d0ef9d58ddbf79292e
Syed Essam Uddin Khawaja <syedessamuddinkhawaja@Mac-2.lan>
2025-01-03 18:06:48 -0700

    Minor fixes and wadood added to team

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import tempImage from "./images/Your paragraph text.png";
 import heroImage from "./images/Stress-rafiki.svg";
 import essam from "./images/IMG_9058.jpg";
 import ghazi from "./images/300f7ed0-596a-4505-84e7-8f4d061442f9.jpg";
+import wadood from "./images/EBFE6868-3A03-4C5B-8B4A-CE7FAAE3C9E9_1_201_a.jpeg";
 import youtubeIcon from "./images/icons/youtube_activity_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
 import codeIcon from "./images/icons/code_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
 import socialIcon from "./images/icons/public_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
@@ -66,10 +67,10 @@ function About(props) {
           <div>
             <h2>Essam</h2>
             <p>
-              The founder of AGSE Media with over two years of experience in web
-              design and a year in project management. Creating websites that
-              are tailored to your brand and meeting business goals is never a
-              challenge for him.
+              The co-founder of AGSE Media with over two years of experience in
+              web design and a year in project management. Creating websites
+              that are tailored to your brand and meeting business goals is
+              never a challenge for him.
             </p>
           </div>
           <img src={essam} alt="#" className="profile-picture"></img>
@@ -84,6 +85,17 @@ function About(props) {
               having a knack for planning ad campaigns.
             </p>
           </div>
+        </div>
+        <div className="team-member wadood">
+          <div>
+            <h2>Wadood</h2>
+            <p>
+              A content writer who charms customers to your brand with the help
+              of words. With expertise gained over two years, he writes content
+              that fuels business growth and boosts engagement.
+            </p>
+          </div>
+          <img src={wadood} alt="#" className="profile-picture"></img>
         </div>
       </div>
 
